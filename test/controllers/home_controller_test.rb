8f412cdcf310291635be27cba3922a331414ea33
TheFSilver <dasylva.f@gmail.com>
2018-08-15 00:10:16 +0200

    tests trials

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,20 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should have a 'navbar' div" do
+    get home_index_url
+    assert_select "div.navbar"
+  end
+
+  test "should have a 'navbar-logo' div" do
+    get home_index_url
+    assert_select "div.navbar", "div.navbar-logo"
+  end
+
+  test "should have a 'navbar-right' div" do
+    get home_index_url
+    assert_select "div.navbar", "div.navbar-right"
+  end
+
+
 end
